f39982be83a98d07a269959745cefe0b2db6688a
UmiKami <ggoku02@gmail.com>
2023-02-24 15:03:34 -0500

    solved

--- src/main/java/tech/strategio/E01.java
+++ src/main/java/tech/strategio/E01.java
@@ -3,8 +3,18 @@ package tech.strategio;
 import java.util.*;
 
 public class E01 {
+    public int generateFibSeqHelper( int n){
+        if(n == 1 || n == 0){
+            return n;
+        }
+
+        return generateFibSeqHelper(n-1) + generateFibSeqHelper(n-2);
+    }
+
     public int getNthFib(int n) {
         // FIXME
-        return -1;
+        int nthFib = generateFibSeqHelper(n-1);
+
+        return nthFib;
     }
 }
